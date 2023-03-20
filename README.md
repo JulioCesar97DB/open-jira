ea1b7f34ffcb27413fafbc68bbdd9b496457dc77
JCD <juliocesar97db@gmail.com>
2023-03-20 12:45:57 -0400

    created the model and connection to the db

--- README.md
+++ README.md
@@ -1,12 +1,15 @@
 # Next.js OpenJira App
 Para correr localmente, se necesita la base de datos 
-'''
+```
 docker-compose up -d
-'''
+```
 
-*El -d, significa __detached__
+* El -d, significa __detached__
 
-*MongoDB URL Local:
-'''
+* MongoDB URL Local:
+```
 mongodb://localhost:27017/entriesdb
-'''
+```
+
+## Configurar las variables de entorno
+Renombrar el archivo __.env.template__ a __.env__
---
+++ database/db.ts
@@ -0,0 +1,42 @@
+import mongoose from 'mongoose';
+
+/**
+ * 0 = disconnected
+ * 1 = connected
+ * 2 = connecting
+ * 3 = disconecting
+ */
+
+const mongooConnection = {
+    isConnected: 0
+}
+
+export const connect = async () => {
+    if (mongooConnection.isConnected) {
+        console.log('We are connected');
+        return;
+    }
+
+    if (mongoose.connections.length > 0) {
+        mongooConnection.isConnected = mongoose.connections[0].readyState;
+
+        if (mongooConnection.isConnected === 1) {
+            console.log('Usando conexion anterior');
+            return;
+        }
+
+        await mongoose.disconnect();
+    }
+
+    await mongoose.connect(process.env.MONGO_URL || '');
+
+    mongooConnection.isConnected = 1;
+    console.log('Connected to MongoDB:', process.env.MONGO_URL)
+}
+
+export const disconnect = async() => {
+    if (mongooConnection.isConnected === 0 ) return;
+
+    await mongoose.disconnect();
+    console.log("Disconnected from MongoDB")
+}
